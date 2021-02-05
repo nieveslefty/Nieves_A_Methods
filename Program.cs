00c4705f8e4353a9ee236623d2422d11382f5f95
Alexandra Nieves <alexandranie@usf.edu>
2021-02-05 18:26:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/* Name: Alexandra Nieves
+   Date: 2/5/2021
+   Method assignment
+*/
+
+using System;
+
+namespace Nieves_A_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name:");
+            string userInput = Console.ReadLine();
+            PrintName(userInput);
+            Console.ReadKey();
+        }
+        static void PrintName(string name)
+        {
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
